a0f27075218c05fad54b3d04a041ffcb00e705d1
Th1nhNg0 <ngophuthinh2001@gmail.com>
2024-10-15 01:18:48 +0700

    Refactor scrapy settings and increase concurrent requests

--- banan.py
+++ banan.py
@@ -6,16 +6,26 @@ import os
 class CongbobananSpider(scrapy.Spider):
     name = "banan"
     custom_settings = {
+        "USER_AGENT": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3",
+        "DEFAULT_REQUEST_HEADERS": {
+            "Connection": "close",
+            # Add headers to simulate a real browser, matching the User Agent
+            "Sec-Ch-Ua": '" Not A;Brand";v="99", "Chromium";v="90", "Google Chrome";v="90"',
+            "Sec-Ch-Ua-Mobile": "?0",
+            "Sec-Ch-Ua-Platform": '"Windows"',
+        },
         "FEED_EXPORT_ENCODING": "utf-8",
         "FEED_FORMAT": "jsonlines",
         "FEED_URI": "output/banan.jsonl",
         "LOG_LEVEL": "INFO",
         "JOBDIR": "output/job_data",
-        "CONCURRENT_REQUESTS": 64,
+        "CONCURRENT_REQUESTS": 256,
         "CONCURRENT_REQUESTS_PER_DOMAIN": 1000,
-        "REDIRECT_ENABLED": False,
         "COOKIES_ENABLED": False,
-        "ROBOTSTXT_OBEY": False,
+        "DOWNLOAD_DELAY": 0.5,
+        "DOWNLOADER_MIDDLEWARES": {
+            "scrapy.downloadermiddlewares.retry.RetryMiddleware": None,
+        },
     }
 
     def __init__(self, *args, **kwargs):
@@ -24,9 +34,17 @@ class CongbobananSpider(scrapy.Spider):
         if not os.path.exists(self.pdf_folder):
             os.makedirs(self.pdf_folder)
 
-    start_urls = [
-        f"https://congbobanan.toaan.gov.vn/2ta{i}t1cvn/" for i in range(1, 1700000)
-    ]
+    # start_urls = [
+    #     f"https://congbobanan.toaan.gov.vn/2ta{i}t1cvn/" for i in range(1, 1700000)
+    # ]
+
+    def start_requests(self):
+        for i in range(1, 1700000):
+            yield Request(
+                url=f"https://congbobanan.toaan.gov.vn/2ta{i}t1cvn/",
+                callback=self.parse,
+                dont_filter=True,
+            )
 
     def parse(self, response):
         title = response.css("title::text").extract_first().strip()
@@ -61,9 +79,3 @@ class CongbobananSpider(scrapy.Spider):
         #     priority=10,  # Higher priority for downloading PDFs
         #     dont_filter=True,
         # )
-
-    def download_pdf(self, response):
-        record_id = response.meta["record_id"]
-        pdf_path = os.path.join(self.pdf_folder, f"{record_id}.pdf")
-        with open(pdf_path, "wb") as f:
-            f.write(response.body)
